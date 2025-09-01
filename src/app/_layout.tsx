7ffbdc710b2757563ae316fdd7cadcd4a592b6b1
alireza <a.mahmoudi@parslib.com>
2025-09-01 19:17:51 +0330

    Update RootLayout to customize Stack screen options with centered header title.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,11 +1,17 @@
 import { Stack } from "expo-router/stack";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { KeyboardProvider } from "react-native-keyboard-controller";
+
 export default function RootLayout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <KeyboardProvider>
-        <Stack />
+        <Stack
+          screenOptions={{
+            headerTitle: "",
+            headerTitleAlign: "center",
+          }}
+        />
       </KeyboardProvider>
     </GestureHandlerRootView>
   );
