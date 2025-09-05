220895af524eff161a2b61ad1fd6b90e82146878
alireza <a.mahmoudi@parslib.com>
2025-09-05 18:16:05 +0330

    Add internationalization support with Typesafe I18n and update dependencies. Integrated Typesafe I18n in the layout for locale management, added new dependencies for formatjs localization, and updated package configurations. Enhanced loading experience with a font loader and activity indicator during initialization.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,12 @@
 import { ThemeProvider } from "@/hooks";
+import TypesafeI18n from "@/i18n/i18n-react";
+import { loadAllLocales } from "@/i18n/i18n-util.sync";
+import "@/polyfill";
 import Feather from "@expo/vector-icons/Feather";
+import { useFonts } from "expo-font";
 import { Stack, useRouter } from "expo-router";
+import { useEffect } from "react";
+import { ActivityIndicator } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { KeyboardProvider } from "react-native-keyboard-controller";
 
@@ -18,23 +24,41 @@ const HeaderRight = (props: any) => {
   );
 };
 
+const Layout = () => {
+  const [loaded] = useFonts({
+    ...Feather.font,
+  });
+  useEffect(() => {
+    loadAllLocales();
+  }, []);
+
+  if (!loaded) {
+    return <ActivityIndicator size="large" color="green" />;
+  }
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#F6F8FA",
+        },
+        headerTitle: "",
+        headerTitleAlign: "center",
+        headerShadowVisible: false,
+        headerBackButtonDisplayMode: "minimal",
+        headerRight: HeaderRight,
+      }}
+    />
+  );
+};
+
 export default function RootLayout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <KeyboardProvider>
         <ThemeProvider>
-          <Stack
-            screenOptions={{
-              headerStyle: {
-                backgroundColor: "#F6F8FA",
-              },
-              headerTitle: "",
-              headerTitleAlign: "center",
-              headerShadowVisible: false,
-              headerBackButtonDisplayMode: "minimal",
-              headerRight: HeaderRight,
-            }}
-          />
+          <TypesafeI18n locale="fa">
+            <Layout />
+          </TypesafeI18n>
         </ThemeProvider>
       </KeyboardProvider>
     </GestureHandlerRootView>
