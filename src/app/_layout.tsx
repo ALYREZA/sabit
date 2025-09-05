160e83a86bb2e79d292f413ba16728eab030c0c6
alireza <a.mahmoudi@parslib.com>
2025-09-05 15:50:47 +0330

    Enhance RootLayout by adding custom header right button for navigation. Updated Stack screen options to include a styled header with a minimal back button and centered title. Improved keyboard handling with GestureHandlerRootView and KeyboardProvider integration.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,14 +1,40 @@
 import { ThemeProvider } from "@/hooks";
-import { Stack } from "expo-router/stack";
+import Feather from "@expo/vector-icons/Feather";
+import { Stack, useRouter } from "expo-router";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { KeyboardProvider } from "react-native-keyboard-controller";
+
+const HeaderRight = (props: any) => {
+  const { canGoBack, back } = useRouter();
+  return (
+    canGoBack() && (
+      <Feather
+        name="arrow-right"
+        size={24}
+        color={props.tintColor}
+        onPress={back}
+      />
+    )
+  );
+};
 
 export default function RootLayout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <KeyboardProvider>
         <ThemeProvider>
-          <Stack />
+          <Stack
+            screenOptions={{
+              headerStyle: {
+                backgroundColor: "#F6F8FA",
+              },
+              headerTitle: "",
+              headerTitleAlign: "center",
+              headerShadowVisible: false,
+              headerBackButtonDisplayMode: "minimal",
+              headerRight: HeaderRight,
+            }}
+          />
         </ThemeProvider>
       </KeyboardProvider>
     </GestureHandlerRootView>
